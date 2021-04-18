5d028e0ae70e8348f623dc64cd943dd483172b4f
Mitchell Garrett <45768600+mitchellgarrett@users.noreply.github.com>
2021-04-18 18:20:30 -0500

    Save/load system

--- decision_matrix.js
+++ decision_matrix.js
@@ -1,3 +1,4 @@
+const ID_MATRIX_TITLE = "MATRIX_TITLE";
 const ID_NUM_FEASIBILITY_FACTORS = "NUM_FEASIBILITY_FACTORS";
 const ID_NUM_IMPACT_FACTORS = "NUM_IMPACT_FACTORS";
 const ID_NUM_OPTIONS = "NUM_OPTIONS";
@@ -12,19 +13,118 @@ const MIN_VALUE = 0;
 const MAX_VALUE = 4;
 
 var DATA = {
-	"num_feasibility_factors" : "3",
-	"num_impact_factors" : "3",
-	"num_options" : "3",
-	"feasibility_factors" : [],
-	"impact_factors" : [],
-	"options" : [],
-	"weights" : [],
-	"feasibility_values" : [],
-	"impact_values" : []
+	"title" : "Decision Matrix",
+	"num_feasibility_factors" : "1",
+	"num_impact_factors" : "1",
+	"num_options" : "1",
+	"feasibility_labels" : [ "Factor 1" ],
+	"impact_labels" : [ "Factor 1" ],
+	"option_labels" : [ "Option 1" ],
+	"feasibility_weights" : [ `${MAX_VALUE / 2 - MIN_VALUE}` ],
+	"impact_weights" : [ `${MAX_VALUE / 2 - MIN_VALUE}` ],
+	"feasibility_values" : [ `${MAX_VALUE / 2 - MIN_VALUE}` ],
+	"impact_values" : [ `${MAX_VALUE / 2 - MIN_VALUE}` ]
 };
 
-function saveData(data) {
-	window.localStorage.setItem(ID_DATA, JSON.stringify(data));
+function clearData() {
+	window.localStorage.removeItem(ID_DATA);
+	location.reload();
+}
+
+function onDataChanged() {
+	// Set title and length variables
+	DATA.title = document.getElementById(ID_MATRIX_TITLE).value;
+	
+	const num_feasibility_factors = parseInt(document.getElementById(ID_NUM_FEASIBILITY_FACTORS).value);
+	DATA.num_feasibility_factors = num_feasibility_factors;
+	
+	const num_impact_factors = parseInt(document.getElementById(ID_NUM_IMPACT_FACTORS).value);
+	DATA.num_impact_factors = num_impact_factors;
+	
+	const num_options = parseInt(document.getElementById(ID_NUM_OPTIONS).value);
+	DATA.num_options = num_options;
+	
+	var element;
+	
+	DATA.feasibility_labels = [];
+	DATA.feasibility_weights = [];
+	for (var f = 0; f < num_feasibility_factors; f++) {
+		// Set label
+		element = document.getElementById(`LABEL_F_${f}`);
+		if (element != null) {
+			DATA.feasibility_labels[f] = element.value;
+		} else {
+			DATA.feasibility_labels[f] = `Factor ${(f + 1)}`;
+		}
+		
+		// Set weight
+		element = document.getElementById(`WEIGHT_F_${f}`);
+		if (element != null) {
+			DATA.feasibility_weights[f] = element.value;
+		} else {
+			DATA.feasibility_weights[f] = MAX_VALUE / 2 - MIN_VALUE;
+		}
+	}
+	
+	DATA.impact_labels = [];
+	DATA.impact_weights = [];
+	for (var i = 0; i < num_impact_factors; i++) {
+		// Set label
+		element = document.getElementById(`LABEL_I_${i}`);
+		if (element != null) {
+			DATA.impact_labels[i] = element.value;
+		} else {
+			DATA.impact_labels[i] = `Factor ${(i + 1)}`;
+		}
+		
+		// Set weight
+		element = document.getElementById(`WEIGHT_I_${i}`);
+		if (element != null) {
+			DATA.impact_weights[i] = element.value;
+		} else {
+			DATA.impact_weights[i] = MAX_VALUE / 2 - MIN_VALUE;
+		}
+	}
+	
+	DATA.option_labels = [];
+	DATA.feasibility_values = [];
+	DATA.impact_values = [];
+	for (var o = 0; o < num_options; o++) {
+		// Set label
+		element = document.getElementById(`LABEL_O_${o}`);
+		if (element != null) {
+			DATA.option_labels[o] = element.value;
+		} else {
+			DATA.option_labels[o] = `Option ${(o + 1)}`;
+		}
+		
+		// Set feasibility values
+		DATA.feasibility_values[o] = [];
+		for (var f = 0; f < num_feasibility_factors; f++) {
+			element = document.getElementById(`VALUE_F_${o}_${f}`);
+			if (element != null) {
+				DATA.feasibility_values[o][f] = element.value;
+			} else {
+				DATA.feasibility_values[o][f] = MAX_VALUE / 2 - MIN_VALUE;;
+			}
+		}
+		
+		// Set impact values
+		DATA.impact_values[o] = [];
+		for (var i= 0; i < num_impact_factors; i++) {
+			element = document.getElementById(`VALUE_I_${o}_${i}`);
+			if (element != null) {
+				DATA.impact_values[o][i] = element.value;
+			} else {
+				DATA.impact_values[o][i] = MAX_VALUE / 2 - MIN_VALUE;;
+			}
+		}
+	}
+}
+
+function saveData() {
+	onDataChanged();
+	window.localStorage.setItem(ID_DATA, JSON.stringify(DATA));
 }
 
 function loadData() {
@@ -32,16 +132,16 @@ function loadData() {
 	if (data != null) {
 		DATA = JSON.parse(data);
 	}
+	
+	generateMatrix();
 }
 
 function initializeMatrix() {
 	loadData();
-	generateMatrix();
 }
 
-function calculateSubMatrix(factorType, id) {
-	var factors = document.getElementById(factorType).value;
-	var options = document.getElementById(ID_NUM_OPTIONS).value;
+function calculateSubMatrix(factors, id) {
+	var options = DATA.num_options;
 	
 	var total = 0;
 	for (var f = 0; f < factors; f++) {
@@ -54,11 +154,15 @@ function calculateSubMatrix(factorType, id) {
 		var sum = 0;
 		for (var f = 0; f < factors; f++) {
 			var weight = parseInt(document.getElementById(`WEIGHT_${id}_${f}`).value);
-			var value = parseInt(document.getElementById(`VALUE_${id}_${f}_${o}`).value);
+			var value = parseInt(document.getElementById(`VALUE_${id}_${o}_${f}`).value);
 			sum += weight * (value - MIN_VALUE);
 		}
 		
-		values.push(sum / total);
+		var value = 0;
+		if (total > 0) {
+			value = sum / total;
+		}
+		values.push(value);
 		document.getElementById(`OUTPUT_${id}_${o}`).value = Math.round(values[values.length - 1] * 100) + "%";
 	}
 	
@@ -126,17 +230,13 @@ function drawGraph(xs, ys) {
 }
 
 function calculateMatrix() {
-	xs = calculateSubMatrix(ID_NUM_FEASIBILITY_FACTORS, "F");
-	ys = calculateSubMatrix(ID_NUM_IMPACT_FACTORS, "I");
+	xs = calculateSubMatrix(DATA.num_feasibility_factors, "F");
+	ys = calculateSubMatrix(DATA.num_impact_factors, "I");
 	drawGraph(xs, ys);
 }
 
-function generateSubMatrix(matrixType, factorType, title, id) {
-	var factors = document.getElementById(factorType).value;
-	var options = document.getElementById(ID_NUM_OPTIONS).value;
-	
-	window.localStorage.setItem(factorType, factors);
-	window.localStorage.setItem(ID_NUM_OPTIONS, options);
+function generateSubMatrix(matrixType, factors, title, id) {
+	var options = DATA.num_options;
 	
 	var matrix = document.getElementById(matrixType);
 	matrix.innerHTML = "";
@@ -159,7 +259,7 @@ function generateSubMatrix(matrixType, factorType, title, id) {
 	// Columns
 	for (var o = 0; o < options; o++) {
 		th = document.createElement("th");
-		th.innerHTML = `<input type="text" value="Option ${(o + 1)}">`;
+		th.innerHTML = `<input id="LABEL_O_${o}" type="text" value="${DATA.option_labels[o]}" oninput="onDataChanged();">`;
 		tr.appendChild(th);
 	}
 	
@@ -196,9 +296,14 @@ function generateSubMatrix(matrixType, factorType, title, id) {
 }
 
 function generateMatrix() {
-	saveData(DATA);
-	generateSubMatrix(ID_FEASIBILITY_MATRIX, ID_NUM_FEASIBILITY_FACTORS, "Feasibility Factors", "F");
-	generateSubMatrix(ID_IMPACT_MATRIX, ID_NUM_IMPACT_FACTORS, "Impact Factors", "I");
+	//saveData(DATA);
+	document.getElementById(ID_MATRIX_TITLE).value = DATA.title;
+	document.getElementById(ID_NUM_FEASIBILITY_FACTORS).value = DATA.num_feasibility_factors;
+	document.getElementById(ID_NUM_IMPACT_FACTORS).value = DATA.num_impact_factors;
+	document.getElementById(ID_NUM_OPTIONS).value = DATA.num_options;
+	
+	generateSubMatrix(ID_FEASIBILITY_MATRIX, DATA.num_feasibility_factors, "Feasibility Factors", "F");
+	generateSubMatrix(ID_IMPACT_MATRIX, DATA.num_impact_factors, "Impact Factors", "I");
 	calculateMatrix();
 }
 
@@ -208,22 +313,42 @@ function generateRow(f, options, id) {
 	// Title
 	var th;
 	th = document.createElement("th");
-	th.innerHTML = `<input type="text" value="Factor ${(f + 1)}">`;
+	var label = `Factor ${(f + 1)}`;
+	if (id == "F") {
+		label = DATA.feasibility_labels[f];
+	} else if (id == "I") {
+		label = DATA.impact_labels[f];
+	}
+	th.innerHTML = `<input id="LABEL_${id}_${f}" type="text" value="${label}" oninput="onDataChanged();">`;
 	tr.appendChild(th);
 	
 	// Weight slider
-	tr.appendChild(generateSlider(`WEIGHT_${id}_${f}`));
+	var value;
+	value = MAX_VALUE / 2 - MIN_VALUE;
+	if (id == "F") {
+		value = parseInt(DATA.feasibility_weights[f]);
+	} else if (id == "I") {
+		value = parseInt(DATA.impact_weights[f]);
+	}
+	tr.appendChild(generateSlider(`WEIGHT_${id}_${f}`, value));
 	
 	// Value sliders
 	for (var o = 0; o < options; o++) {
-		tr.appendChild(generateSlider(`VALUE_${id}_${f}_${o}`));
+		value = MAX_VALUE / 2 - MIN_VALUE;
+		if (id == "F") {
+			value = parseInt(DATA.feasibility_values[o][f]);
+		} else if (id == "I") {
+			value = parseInt(DATA.impact_values[o][f]);
+		}
+		tr.appendChild(generateSlider(`VALUE_${id}_${o}_${f}`, value));
 	}
 	
 	return tr;
 }
 
-function generateSlider(id) {
+function generateSlider(id, value) {
 	th = document.createElement("th");
-	th.innerHTML = `<output>${LABELS[MAX_VALUE / 2 - MIN_VALUE]}</output> <br> ${LABELS[MIN_VALUE]} <input id="${id}" type="range" max="${MAX_VALUE}" min="${MIN_VALUE}" oninput="this.previousElementSibling.previousElementSibling.value = LABELS[this.value]; calculateMatrix();"> ${LABELS[MAX_VALUE]}`;
+	th.innerHTML = `<output>${LABELS[value]}</output> <br> ${LABELS[MIN_VALUE]} <input id="${id}" type="range" max="${MAX_VALUE}" min="${MIN_VALUE}" oninput="this.previousElementSibling.previousElementSibling.value = LABELS[this.value]; onDataChanged(); calculateMatrix();"> ${LABELS[MAX_VALUE]}`;
+	th.childNodes[4].value = value;
 	return th;
 }
